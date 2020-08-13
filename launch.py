be226c20b222739c61c6c5174441bd94ffbac37a
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-08-13 20:24:07 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,10 +1,10 @@
 import tweepy
 from config import *
-from streamListener import *
 
 auth = tweepy.OAuthHandler(consumer_token, consumer_secret_token)
 auth.set_access_token(access_token, access_secret_token)
 api = tweepy.API(auth)
 
-stream = tweepy.Stream(api = api.auth, listener = StreamListener())
+streamListener = StreamListener()
+stream = tweepy.Stream(api = api.auth, listener = streamListener)
 stream.userstream()
