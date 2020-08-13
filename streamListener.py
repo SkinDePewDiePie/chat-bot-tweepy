2e16258d953bfe84ef1eea3c41bbee84fb1ffd3f
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-08-13 19:57:03 +0200

    Create streamListener.py

---
+++ streamListener.py
@@ -0,0 +1,22 @@
+import tweepy
+from config import prefix
+from launch import api
+
+class StreamListener(tweepy.StreamListener):
+    def on_error(self, error_code):
+        if error_code == 420:
+             return False
+        else:
+             return True
+
+    def on_status(self, status):
+        print(prefix + status.text)
+        return True
+
+    def on_data(self, data):
+        print(prefix + data.text)
+        return True
+
+    def on_direct_message(self, direct_message):
+        api.send_direct_message(recipient_id = direct_message.author.sender_id, text = "This is a response fro Tweepy !")
+        return True
